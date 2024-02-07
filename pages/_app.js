6ac138d97584807d42a688e5b8a8fefb3f51a3b2
andruslan17 <157966769+andruslan17@users.noreply.github.com>
2024-02-07 23:08:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -429,66 +429,66 @@ const abi = [
 const contractAddress = '0x694E31fB6cf8E86Bb09e67D58b82B5abc6C2065E';
 
 const { chains, provider } = configureChains(
-	[
-		mainnet,
-		goerli,
-		polygon,
-		polygonMumbai,
-		optimism,
-		optimismGoerli,
-		arbitrum,
-		arbitrumGoerli,
-	],
-	[alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]
+    [
+        mainnet,
+        goerli,
+        polygon,
+        polygonMumbai,
+        optimism,
+        optimismGoerli,
+        arbitrum,
+        arbitrumGoerli,
+    ],
+    [alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-	appName: "My Alchemy DApp",
-	chains,
+    appName: "My Alchemy DApp",
+    chains,
 });
 
 const wagmiClient = createClient({
-	autoConnect: true,
-	connectors,
-	provider,
+    autoConnect: true,
+    connectors,
+    provider,
 });
 
 const callSmartContract = async () => {
-	try {
-		const response = await fetch(`https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&format=json`);
-		const { result: abi } = await response.json();
-		const contractInstance = new provider.eth.Contract(JSON.parse(abi), contractAddress);
-		const result = await contractInstance.methods.methodName().call();
-		console.log('Результат вызова функции смарт-контракта:', result);
-	} catch (error) {
-		console.error('Произошла ошибка при вызове смарт-контракта:', error);
-	}
+    try {
+        const response = await fetch(`https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&format=json`);
+        const { result: abi } = await response.json();
+        const contractInstance = new provider.eth.Contract(JSON.parse(abi), contractAddress);
+        const result = await contractInstance.methods.methodName().call();
+        console.log('Результат вызова функции смарт-контракта:', result);
+    } catch (error) {
+        console.error('Произошла ошибка при вызове смарт-контракта:', error);
+    }
 };
 
 function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		if (typeof window.ethereum === 'undefined') {
-		  window.ethereum = {};
-		}
-		
-		if (wagmiClient.connected) {
-			callSmartContract();
-		}
-	}, []);
+    useEffect(() => {
+        if (typeof window.ethereum === 'undefined') {
+            window.ethereum = {};
+        }
 
-	return (
-		<WagmiConfig client={wagmiClient}>
-			<RainbowKitProvider
-				modalSize="compact"
-				initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
-				chains={chains}
-			>
-				<MainLayout>
-					<Component {...pageProps} />
-				</MainLayout>
-			</RainbowKitProvider>
-		</WagmiConfig>
-	);
+        if (wagmiClient.connected) {
+            callSmartContract();
+        }
+    }, [wagmiClient.connected]);
+
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider
+                modalSize="compact"
+                initialChain={polygon.name} // Установить начальную сеть на Polygon
+                chains={chains}
+            >
+                <MainLayout>
+                    <Component {...pageProps} />
+                </MainLayout>
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
 }
 
 export { WagmiConfig, RainbowKitProvider };
